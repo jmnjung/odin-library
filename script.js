667c172557d067e30e5fd97daea84d7ee6453740
Jimin Jung <170469689+jmnjung@users.noreply.github.com>
2025-02-08 13:56:05 -0800

    Add first form validation with JavaScript

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const container = document.querySelector(".container");
 const dialog = document.querySelector("dialog");
 const form = document.querySelector("form");
 const titleInput = document.querySelector("#book-title");
+const titleError = document.querySelector("#book-title + .error");
 const authorInput = document.querySelector("#book-author");
+const authorError = document.querySelector("#book-author + .error");
 const pagesInput = document.querySelector("#book-pages");
+const pagesError = document.querySelector("#book-pages + .error");
 const readInput = document.querySelector("#book-read");
 const addBtn = document.querySelector("dialog + button");
 const closeBtn = document.querySelector(".close-dialog");
@@ -37,7 +40,8 @@ function displayLibrary() {
 
     const pagesDiv = document.createElement("div");
     pagesDiv.classList.add("book-pages");
-    pagesDiv.textContent = `${book.numPages} pages`;
+    pagesDiv.textContent =
+      book.numPages > 1 ? `${book.numPages} pages` : `${book.numPages} page`;
 
     const textDiv = document.createElement("div");
     textDiv.classList.add("book-description");
@@ -82,23 +86,104 @@ function addBookToLibrary(title, author, numPages, isRead) {
   displayLibrary();
 }
 
+function showTitleError() {
+  titleError.classList.add("active");
+
+  if (titleInput.validity.valueMissing) {
+    titleError.textContent = "The title cannot be empty.";
+  } else if (titleInput.validity.patternMismatch) {
+    titleError.textContent =
+      "The title cannot have leading or trailing spaces.";
+  }
+}
+
+function showAuthorError() {
+  authorError.classList.add("active");
+
+  if (authorInput.validity.valueMissing) {
+    authorError.textContent = "The author cannot be empty.";
+  } else if (authorInput.validity.patternMismatch) {
+    authorError.textContent =
+      "The author name cannot have leading or trailing spaces.";
+  }
+}
+
+function showPagesError() {
+  pagesError.classList.add("active");
+
+  if (pagesInput.validity.valueMissing) {
+    pagesError.textContent = "The number of pages cannot be empty.";
+  } else if (pagesInput.validity.patternMismatch) {
+    pagesError.textContent = "The number of pages must be a positive integer.";
+  }
+}
+
 addBtn.addEventListener("click", () => {
   dialog.showModal();
 });
 
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   form.reset();
   dialog.close();
 });
 
-form.addEventListener("submit", () => {
-  addBookToLibrary(
-    titleInput.value,
-    authorInput.value,
-    parseInt(pagesInput.value),
-    readInput.checked
-  );
-  form.reset();
+titleInput.addEventListener("input", () => {
+  if (titleInput.validity.valid) {
+    titleError.classList.remove("active");
+    titleError.textContent = "";
+  } else {
+    showTitleError();
+  }
+});
+
+authorInput.addEventListener("input", () => {
+  if (authorInput.validity.valid) {
+    authorError.classList.remove("active");
+    authorError.textContent = "";
+  } else {
+    showAuthorError();
+  }
+});
+
+pagesInput.addEventListener("input", () => {
+  if (pagesInput.validity.valid) {
+    pagesError.classList.remove("active");
+    pagesError.textContent = "";
+  } else {
+    showPagesError();
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (
+    titleInput.validity.valid &&
+    authorInput.validity.valid &&
+    pagesInput.validity.valid
+  ) {
+    addBookToLibrary(
+      titleInput.value,
+      authorInput.value,
+      parseInt(pagesInput.value),
+      readInput.checked
+    );
+    form.reset();
+    dialog.close();
+  } else {
+    if (!titleInput.validity.valid) {
+      showTitleError();
+    }
+
+    if (!authorInput.validity.valid) {
+      showAuthorError();
+    }
+
+    if (!pagesInput.validity.valid) {
+      showPagesError();
+    }
+  }
 });
 
 addBookToLibrary("Atomic Habits", "James Clear", 306, true);
